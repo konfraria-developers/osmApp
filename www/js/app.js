09581fa6cf9436f7c8f15399c60589b58c0b058b
jaume.figueras <jaume.figueras@upc.edu>
2015-07-11 17:45:53 +0200

    translations

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'pascalprecht.translate', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -19,7 +19,15 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   });
 })
-
+.config(['$translateProvider', function($translateProvider) {
+  $translateProvider.useStaticFilesLoader({
+    prefix: 'translations/locale-',
+    suffix: '.json'
+  });
+  $translateProvider.determinePreferredLanguage();
+  $translateProvider.fallbackLanguage(['en']);
+  $translateProvider.useSanitizeValueStrategy('escaped');
+}])
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 

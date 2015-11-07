c7eb6b387d7dc0feea001347e5f35acbd18d8a2f
edgarriba <edgar.riba@gmail.com>
2015-11-07 14:36:56 +0100

    add marker rendering

--- www/js/controllers/map.js
+++ www/js/controllers/map.js
@@ -11,7 +11,12 @@ angular.module('starter.controllers')
       lng: 1.9686877,
       zoom: 18
     },
-    markers: {},
+    markers: {
+      item: {
+        lat: null,
+        lng: null
+      }
+    },
     layers: {},
     defaults: {
       tileLayer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
@@ -26,11 +31,21 @@ angular.module('starter.controllers')
     }
   });
 
+  $scope.$watch('DATA.selectedItem', function() {
+
+    $scope.markers.item.lat = $scope.DATA.selectedItem.geometry.coordinates[1]
+    $scope.markers.item.lng = $scope.DATA.selectedItem.geometry.coordinates[0]
+
+    $scope.center.lat = $scope.markers.item.lat
+    $scope.center.lng = $scope.markers.item.lng
+
+  });
+
+/*
   $scope.setMarker = function(item) {
 
     // add marker to markers array
 
-    $scope.markers = new Array();
     $scope.markers.push({
       lat: item.lat,
       lng: item.lon,
@@ -49,5 +64,6 @@ angular.module('starter.controllers')
     console.log( $scope.center );
 
   }
+*/
 
 }]);
